ad863a43a1ae59082950459839686b8c311caf8b
archilleu <archilleu9527@gmail.com>
2020-04-29 17:22:56 +0800

    修改配置

--- src/main/java/com/haoya/demo/common/config/ConfigParam.java
+++ src/main/java/com/haoya/demo/common/config/ConfigParam.java
@@ -6,23 +6,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConfigParam {
 
-    public String getBackgroundPath() {
-        return backgroundPath;
-    }
-
     public String getUploadPath() {
         return uploadPath;
     }
-
-    public String getLogoPath() {
-        return logoPath;
-    }
-
-    @Value("${system.logo-path}")
-    private String logoPath;
-
-    @Value("${system.background-path}")
-    private String backgroundPath;
 
     @Value("${system.upload-path}")
     private String uploadPath;
