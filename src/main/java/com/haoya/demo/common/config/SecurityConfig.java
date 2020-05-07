1f831445a636f88949d457760796ce872a2484e1
archilleu <archilleu9527@gmail.com>
2020-05-07 17:25:43 +0800

    增加remember me功能

--- src/main/java/com/haoya/demo/common/config/SecurityConfig.java
+++ src/main/java/com/haoya/demo/common/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import com.haoya.demo.app.sys.repository.SysRoleRepository;
 import com.haoya.demo.app.sys.repository.SysUserRepository;
 import com.haoya.demo.app.sys.repository.SysUserRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,10 +23,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -62,6 +66,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessHandler(hyLogoutSuccessHandler)
                 .permitAll()
                 .and()
+                .rememberMe()
+                .tokenRepository(persistentTokenRepository())
+                /**
+                 * 归根结底都是缓存记录。用session和用redis方案的区别有两个：
+                 * 1.一个用web服务器的缓存，一个用单独的服务器缓存，涉及到两种不同的运维方式。
+                 * 2.用session直接支持对象存储，用redis需要再做一层对象与redis的数据类型的转换。
+                 */
+                .tokenValiditySeconds(60*60*24) //remember me 过期时间
+                .and()
                 .csrf()
                 .disable()
                 .headers()
@@ -69,26 +82,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sameOrigin()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
-            //ajax请求返回401，web返回301
-            @Override
-            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
-                if (isAjaxRequest(request)) {
-                    response.setHeader("X-Redirect", "/login");
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
-                } else {
-                    response.sendRedirect("/login");
-                }
-            }
+                    //ajax请求返回401，web返回301
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+                        if (isAjaxRequest(request)) {
+                            response.setHeader("X-Redirect", "/login");
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+                        } else {
+                            response.sendRedirect("/login");
+                        }
+                    }
 
-            public boolean isAjaxRequest(HttpServletRequest request) {
-                String ajaxFlag = request.getHeader("X-Requested-With");
-                return ajaxFlag != null && "XMLHttpRequest".equals(ajaxFlag);
-            }
-        });
-        //.and()
-        //    .requiresChannel()
-        //    .antMatchers("/**").requiresSecure()
-        ;
+                    public boolean isAjaxRequest(HttpServletRequest request) {
+                        String ajaxFlag = request.getHeader("X-Requested-With");
+                        return ajaxFlag != null && "XMLHttpRequest".equals(ajaxFlag);
+                    }
+                });
     }
 
     @Override
@@ -125,6 +134,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         security.ignoring().antMatchers("/favicon.ico", "/thirdparty/**", "/common/**");
         //security.ignoring().antMatchers("/**");
     }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl persistentTokenRepository = new JdbcTokenRepositoryImpl();
+        persistentTokenRepository.setDataSource(dataSource);
+        return persistentTokenRepository;
+    }
+
+    @Autowired
+    private DataSource dataSource;
 
     @Autowired
     SysUserRepository sysUserRepository;
