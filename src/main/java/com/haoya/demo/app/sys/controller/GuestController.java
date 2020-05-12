773429d3f2b2d96cedbff113fcd1f0044c7483b6
archilleu <archilleu9527@gmail.com>
2020-05-12 14:18:42 +0800

    添加重试AOP

--- src/main/java/com/haoya/demo/app/sys/controller/GuestController.java
+++ src/main/java/com/haoya/demo/app/sys/controller/GuestController.java
@@ -1,8 +1,12 @@
 package com.haoya.demo.app.sys.controller;
 
+import com.haoya.demo.app.exception.AppExceptionServerError;
+import com.haoya.demo.common.annotation.TryAgain;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/guest")
@@ -13,4 +17,20 @@ public class GuestController {
         return "guest";
     }
 
+    @TryAgain(tryTimes = 5,  handleException="ex", beforeExceptionalReturn="before", shouldRetry="retry")
+    @GetMapping("test")
+    public @ResponseBody String test() throws Exception{
+        throw new Exception("test retry",null);
+    }
+
+    private void ex() {
+        System.out.println("ex");
+    }
+    private void before() {
+        System.out.println("before");
+    }
+
+    private boolean retry() {
+        return true;
+    }
 }
