db2606bd6de5abb86d455c6416eec42a0a353c41
archilleu <archilleu9527@gmail.com>
2020-05-14 16:20:53 +0800

    添加异常处理

--- src/main/java/com/haoya/demo/app/exception/AppExceptionControllerHandler.java
+++ src/main/java/com/haoya/demo/app/exception/AppExceptionControllerHandler.java
@@ -4,9 +4,12 @@ package com.haoya.demo.app.exception;
  * REST api内部定义异常处理控制器
  */
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class AppExceptionControllerHandler {
@@ -15,6 +18,13 @@ public class AppExceptionControllerHandler {
     public ResponseEntity<AppError> appException(AppException ex) {
         AppError error = new AppError(ex.getStatus().value(), ex.getMessage());
         ResponseEntity<AppError> entity = new ResponseEntity<AppError>(error, ex.getStatus());
+        return entity;
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<AppError> exception(IllegalArgumentException ex) {
+        AppError error = new AppError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        ResponseEntity<AppError> entity = new ResponseEntity<AppError>(error, HttpStatus.BAD_REQUEST);
         return entity;
     }
 }
